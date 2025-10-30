88f9ca6cde98979535696840e006b3fb25f52a04
Eugenio <eugenio28.04@gmail.com>
2025-10-30 09:43:07 -0300

    feat: create Status enum

--- src/main/java/io/github/eugod/Main.java
+++ src/main/java/io/github/eugod/Main.java
@@ -1,7 +1,9 @@
 package io.github.eugod;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(Arrays.toString(args));
     }
 }
---
+++ src/main/java/io/github/eugod/enums/Status.java
@@ -0,0 +1,7 @@
+package io.github.eugod.enums;
+
+public enum Status {
+    TODO,
+    IN_PROGRESS,
+    DONE
+}
